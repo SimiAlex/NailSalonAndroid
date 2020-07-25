a30bab867f7f49bfbac12ca81906116f4bce0453
remcv <remuluscatana@tutanota.com>
2020-07-25 09:55:42 +0300

    UI tweaks to appointment_item.xml

--- app/src/main/java/alexsimi/com/github/nailsalon/view/AppointmentAdapter.java
+++ app/src/main/java/alexsimi/com/github/nailsalon/view/AppointmentAdapter.java
@@ -8,7 +8,9 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import alexsimi.com.github.nailsalon.R;
 import alexsimi.com.github.nailsalon.controller.DatabaseHandler;
@@ -20,11 +22,17 @@ public class AppointmentAdapter extends BaseAdapter {
     private Activity myActivity;
     private DatabaseHandler db;
 
+    // fields = DateTimeFormatter
+    private DateTimeFormatter dtf_date;
+    private DateTimeFormatter dtf_time;
+
     //constructor
     public AppointmentAdapter(Activity myActivity, DatabaseHandler db)
     {
         this.myActivity = myActivity;
         this.db = db;
+        dtf_date = DateTimeFormatter.ofPattern("dd.MM.yy");
+        dtf_time = DateTimeFormatter.ofPattern("hh:mm");
     }
 
     //methods from parent
@@ -56,7 +64,8 @@ public class AppointmentAdapter extends BaseAdapter {
 
         TextView id_app = oneAppointmentView.findViewById(R.id.id_tv);
         TextView name_app = oneAppointmentView.findViewById(R.id.name_tv);
-        TextView date_time_app = oneAppointmentView.findViewById(R.id.date_tv);
+        TextView date_app = oneAppointmentView.findViewById(R.id.date_tv);
+        TextView time_app = oneAppointmentView.findViewById(R.id.time_tv);
         TextView procedure_app = oneAppointmentView.findViewById(R.id.procedure_tv);
         TextView price_app = oneAppointmentView.findViewById(R.id.price_tv);
 
@@ -65,9 +74,13 @@ public class AppointmentAdapter extends BaseAdapter {
         id_app.setText(Integer.toString(app.getId()));
         name_app.setText(app.getName());
         Log.d("NailSalon", "getView: " + app.getTime().toString());
-        date_time_app.setText(app.getTime().toString());
+
+        // date and time setup
+        date_app.setText(dtf_date.format(app.getTime()));
+        time_app.setText(dtf_time.format(app.getTime()));
+
         procedure_app.setText(app.getProcedure());
-        price_app.setText(Double.toString(app.getPrice()));
+        price_app.setText(String.format("%.0f", app.getPrice()));
 
         return oneAppointmentView;
     }
