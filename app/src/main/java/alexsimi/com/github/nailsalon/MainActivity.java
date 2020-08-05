1f944b60b916f0ffb49abf20d1a9d08fec6c6fb3
SimiAlex <simineaalexandru@gmail.com>
2020-08-05 18:19:17 +0300

    Delete activity

--- app/src/main/java/alexsimi/com/github/nailsalon/MainActivity.java
+++ app/src/main/java/alexsimi/com/github/nailsalon/MainActivity.java
@@ -88,6 +88,8 @@ public class MainActivity extends AppCompatActivity
                 onUpdateButtonClicked();
             }
         });
+
+        deleteButton.setOnClickListener(v -> onDeleteButtonClicked());
     }
 
     @Override
